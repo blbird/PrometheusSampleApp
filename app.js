1297e0a7689500e384eb41774436d1b57990fad1
YongHun Jeon <blbird@kr.ibm.com>
2019-07-15 13:26:21 +0900

    initial code for prom-client

---
+++ app.js
@@ -0,0 +1,76 @@
+const promClient = require('prom-client');
+const http = require('http');
+const express = require('express');
+
+const PORT = 9100;
+
+const app = express();
+
+let cnt = 0;
+const statusCodes = [200, 201, 400, 403, 404, 500];
+
+const counter = new promClient.Counter({
+  name: 'prom_sample_counter',
+  help: 'prom_sample_counter_help',
+});
+
+const gauge = new promClient.Gauge({
+  name: 'prom_sample_gauge',
+  help: 'prom_sample_gauge_help',
+});
+
+const labeledGauge = new promClient.Gauge({
+  name: 'prom_sample_labeled_gauge',
+  help: 'prom_sample_labeled_gauge_help',
+  labelNames: ['id'],
+}); 
+
+const histogram = new promClient.Histogram({
+  name: 'prom_sample_histogram',
+  help: 'prom_sample_histogram_help',
+  labelNames: ['status_code'],
+  buckets: [20, 40, 60, 80, 100],
+});
+
+const summary = new promClient.Summary({
+  name: 'prom_sample_summary',
+  help: 'prom_sample_summary_help',
+  labelNames: ['status_code'],
+  buckets: [20, 40, 60, 80, 100],
+});
+
+setInterval(() => {
+  // increase counter value
+  counter.inc();
+
+  // increase adn decrease gauge value
+  if (cnt > 10) {
+    gauge.dec();
+  }
+  else {
+    gauge.inc();
+  }
+
+  // set labeled gauage value
+  labeledGauge.labels('10').set(cnt*10);
+  labeledGauge.set({id: '100'}, cnt*100);
+
+  // set histogram value
+  const status = Math.floor(Math.random() * statusCodes.length);
+  const duration = Math.floor(Math.random() * 100);
+
+  console.log('status : ', statusCodes[status]);
+  console.log('duration : ', duration);
+  histogram.labels(statusCodes[status]).observe(duration);
+  summary.labels(statusCodes[status]).observe(duration);
+  cnt++;
+}, 5000);
+
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', promClient.register.contentType)
+  res.end(promClient.register.metrics())
+});
+
+http.createServer(app).listen(PORT, () => {
+  console.log('Your server is listening on http port ', PORT);
+});
